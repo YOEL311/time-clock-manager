00757c57e788bf88967f2942d816a0ba924f2b16
Yoel31 <yoel301@gmail.com>
2021-04-20 14:30:13 +0300

    add chart in time

---
+++ src/component/Times.js
@@ -0,0 +1,40 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Chart from "react-google-charts";
+
+const data2 = [
+  ["Year", "Visitations", { role: "style" }],
+  ["2010", 10, "color: gray"],
+  ["2020", 14, "color: #76A7FA"],
+  ["2030", 16, "color: blue"],
+  ["2040", 22, "stroke-color: #703593; stroke-width: 4; fill-color: #C5A5CF"],
+  [
+    "2050",
+    28,
+    "stroke-color: #871B47; stroke-opacity: 0.6; stroke-width: 8; fill-color: #BC5679; fill-opacity: 0.2",
+  ],
+];
+
+const data = [
+  ["Element", "Density", { role: "style" }],
+  ["Copper", 8.94, "#b87333"], // RGB value
+  ["Silver", 10.49, "silver"], // English color name
+  ["Gold", 19.3, "gold"],
+  ["Platinum", 21.45, "color: #e5e4e2"], // CSS-style declaration
+];
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Chart
+          chartType="ColumnChart"
+          width="100%"
+          height="400px"
+          data={data}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
